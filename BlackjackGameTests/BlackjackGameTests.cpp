c7fc99e10e5b2f5a345dc06f2ab99d157c5fddd2
Tomislav Prstec <toprstec@vsite.hr>
2025-02-23 11:29:26 +0100

    added BlackjackGameTests project.

---
+++ BlackjackGameTests/BlackjackGameTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BlackjackGameTests
+{
+	TEST_CLASS(BlackjackGameTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
